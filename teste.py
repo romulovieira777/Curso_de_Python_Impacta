a4d33ec694cb5712c4cf10760e79093b8846f079
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-07 09:51:09 -0300

    Teste

---
+++ teste.py
@@ -0,0 +1,177 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/Naara Felix/Desktop/Exercicios/ex5.py ==========
+Traceback (most recent call last):
+  File "C:/Users/Naara Felix/Desktop/Exercicios/ex5.py", line 1, in <module>
+    arquivo = open ('pessoas.txt','r')
+FileNotFoundError: [Errno 2] No such file or directory: 'pessoas.txt'
+>>> 
+========== RESTART: C:/Users/Naara Felix/Desktop/Exercicios/ex5.py ==========
+<_io.TextIOWrapper name='pessoas.txt' mode='r' encoding='cp1252'>
+>>> 
+========== RESTART: C:/Users/Naara Felix/Desktop/Exercicios/ex5.py ==========
+JoÃ£o
+
+Maria
+
+Antonio
+
+Marcos
+
+Alexandre
+
+Leonardo
+
+Gabriela
+
+Adriana
+
+Rafael
+
+Aline
+
+Luiza
+
+JoÃ£o
+
+JoÃ£o Carlos
+
+Maria Fernanda
+
+Rossi
+
+Rafael
+
+Daniel
+
+Miguel
+
+Willian
+
+Fernanda
+
+Luiza
+
+Bianca
+
+Simone
+
+JosÃ©
+
+Luiz
+
+Gustavo
+
+Talles
+
+Willian
+
+Caio
+
+Eduardo
+
+RogÃ©rio
+
+Santana
+
+Tiago
+
+Gabriel
+
+Josiel
+
+Marcia
+
+Tiago
+
+Felipe
+
+JoÃ£o Pedro
+
+Karla
+>>> 
+========== RESTART: C:/Users/Naara Felix/Desktop/Exercicios/ex5.py ==========
+>>> ler_arquivo ('pessoas.txt')
+JoÃ£o
+
+Maria
+
+Antonio
+
+Marcos
+
+Alexandre
+
+Leonardo
+
+Gabriela
+
+Adriana
+
+Rafael
+
+Aline
+
+Luiza
+
+JoÃ£o
+
+JoÃ£o Carlos
+
+Maria Fernanda
+
+Rossi
+
+Rafael
+
+Daniel
+
+Miguel
+
+Willian
+
+Fernanda
+
+Luiza
+
+Bianca
+
+Simone
+
+JosÃ©
+
+Luiz
+
+Gustavo
+
+Talles
+
+Willian
+
+Caio
+
+Eduardo
+
+RogÃ©rio
+
+Santana
+
+Tiago
+
+Gabriel
+
+Josiel
+
+Marcia
+
+Tiago
+
+Felipe
+
+JoÃ£o Pedro
+
+Karla
+>>> 
+========== RESTART: C:/Users/Naara Felix/Desktop/Exercicios/ex5.py ==========
+>>> salvar_arquivo ('texteArquivo.txt', 'Testando a escrita de uma arquivo')
